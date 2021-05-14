47e1eb91eeec3d1d2f1874aa12351eb34cb4aa09
Oliver Neuhäusler <oliver-neuhaeusler@gmx.net>
2021-05-14 14:22:50 +0200

    started with the beginning

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
-import './App.css';
+import { useState, useEffect } from 'react';
 
 function App() {
-  return <div></div>;
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch('https://rickandmortyapi.com/api/character')
+      .then((result) => result.json())
+      .then((data) => setCharacters(data.results));
+  }, []);
+  return (
+    <div>
+      <header>
+        <h1>Rick and Morty Character</h1>
+      </header>
+      <main>
+        {characters.map((character) => (
+          <article>
+            <h2>{character.name}</h2>
+            <img src={character.image} alt={character.name} />
+          </article>
+        ))}
+      </main>
+    </div>
+  );
 }
 
 export default App;
